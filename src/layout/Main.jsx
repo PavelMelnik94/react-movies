e2be011e2e92d31fab28c4eae6ca385ebe3c72e9
Pavel <recyclesordie94@gmail.com>
2021-05-22 19:04:01 +0300

    добавил .env.local с api-key.
    запускаю билд проекта.

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,7 @@ import {Movies} from "../components/Movies";
 import Preloader from "../components/Preloader";
 import Search from "../components/Search";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 class Main extends React.Component {
     state = {
@@ -11,7 +12,7 @@ class Main extends React.Component {
     }
 
     componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=207af2f4&s=matrix')
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(r => r.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
 
@@ -19,7 +20,7 @@ class Main extends React.Component {
 
     handlePress = (str, type = 'all') => {
         this.setState({loading: true})
-        fetch(`http://www.omdbapi.com/?apikey=207af2f4&s=${str}${type !== 'all' ? `&type=${type}` : ''}` )
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}` )
             .then(r => r.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
 
